bb6d35a8537ad39004b0cd2323b1b8154380e736
JP Eiler <jp.eiler97@gmail.com>
2021-03-17 20:21:42 -0500

    worked on getting API data

---
+++ develop/script.js
@@ -0,0 +1,16 @@
+//Client ID: a_iIN-AAkwXxP2O6_bWT3g
+
+// API Key: cEhI0X89y--0ThDWWoEcUJ1OJ0s2yhfvWcdwurErDWl5b5JmH8qVHSCfgadoN04WQDtJaOuEUrcqSNk0rJkod1ic25nYvTfJcdcRA409DgSZrCC8j3hBs5K3UKRSYHYx
+
+var apiKey =
+	'cEhI0X89y--0ThDWWoEcUJ1OJ0s2yhfvWcdwurErDWl5b5JmH8qVHSCfgadoN04WQDtJaOuEUrcqSNk0rJkod1ic25nYvTfJcdcRA409DgSZrCC8j3hBs5K3UKRSYHYx';
+var foodApi = 'https://api.yelp.com/v3/businesses/search';
+
+$.ajax({
+	url: 'http://api.yelp.com/business_review_search',
+	dataType: 'jsonp',
+	data: { term: 'restaurant', lat: xxx, long: xxx }, // callback is not necessary
+	success: function(data) {
+		// data is a normal response shown on yelp's API page
+	}
+});
